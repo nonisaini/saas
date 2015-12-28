0921320997f05dd16faa14484cac04cd93815dad
Parmesh Saini <nonisaini@gmail.com>
2015-12-28 05:17:44 +0000

    updated mailer files

--- app/controllers/concerns/contacts_controller.rb
+++ app/controllers/concerns/contacts_controller.rb
@@ -12,10 +12,12 @@ class ContactsController < ApplicationController
             body = params[:contact][:comments]
             
             ContactMailer.contact_email(name, email, body).deliver
-            flash[:success] = 'Message sent'
+            
+            
+            flash[:success] = 'Message sent.'
             redirect_to new_contact_path
         else
-            flash[:danger] = 'Error occured, message has not been sent'
+            flash[:danger] = 'Error occured, message has not been sent.'
             redirect_to new_contact_path
             
         end
